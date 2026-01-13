c745d0784abd6624cd61c38a44366c592538d66c
Valentijn <Valentijnkeijser@gmail.com>
2026-01-13 17:04:43 +0100

    checkflask

--- start.bat
+++ start.bat
@@ -1,41 +1,17 @@
 @echo off
 title Video Compressor Starter
 echo ---------------------------------------------------
-echo  Even geduld, software wordt gecontroleerd...
-echo  (Dit repareert ook als 'pip' niet wordt herkend)
+echo  Even geduld, het systeem wordt klaargemaakt...
+echo  De eerste keer kan dit een paar minuten duren.
 echo ---------------------------------------------------
 
-REM --- POGING 1: Via de Windows Python Launcher (werkt meestal wel) ---
-py -m pip install flask static-ffmpeg
-IF %ERRORLEVEL% EQU 0 GOTO START_WITH_PY
+REM Installeer Flask en static-ffmpeg (inclusief ffmpeg binaire bestanden)
+pip install flask static-ffmpeg
 
-REM --- POGING 2: Via standaard Python commando (als back-up) ---
-python -m pip install flask static-ffmpeg
-IF %ERRORLEVEL% EQU 0 GOTO START_WITH_PYTHON
-
-echo.
-echo ---------------------------------------------------
-echo  FOUTMELDING:
-echo  Python kon niet worden gevonden.
-echo.
-echo  Oplossing:
-echo  1. De-installeer Python.
-echo  2. Installeer Python opnieuw.
-echo  3. Zorg dat je onderaan het vinkje "Add Python to PATH" AANVINKT.
-echo ---------------------------------------------------
-pause
-exit
-
-:START_WITH_PY
-echo Starten via Launcher...
+REM Open de browser automatisch
 start http://127.0.0.1:5000
-py app.py
-pause
-exit
 
-:START_WITH_PYTHON
-echo Starten via Python...
-start http://127.0.0.1:5000
+REM Start de server
 python app.py
+
 pause
-exit
